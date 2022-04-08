c2dc54d4d7024c2c9e37487ef5e70277a097b997
sarahcrozier <crozierfamily@gmail.com>
2022-04-08 10:15:40 -0700

    work done

---
+++ PyPoll.py
@@ -0,0 +1,10 @@
+#The data we need to retrieve
+election_data = open(C:\Users\crozi\OneDrive\Desktop\MOMMY DO NOT TOUCH\CHALLENGES\election-analysis\election-analysis\Resources, 'r')
+#1. The total number of votes cast
+#2. The complete list of candidates who recieved votes
+#3. The percentage of votes wach candidiate won
+#4. The total number of votes each candidate won
+#5. The winner of the election based on popular vote
+
+#Close the file
+election_data.close
